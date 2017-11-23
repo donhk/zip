1b9687003eff5ce78051bf3a4c3c396fed5dfc5f
donhk <hk.fred.0@gmail.com>
2017-11-23 08:19:04 -0600

    base project files

---
+++ src/main/java/com/donhk/Zip/Zip.java
@@ -0,0 +1,7 @@
+package com.donhk.Zip;
+
+public class Zip {
+    public Zip() {
+
+    }
+}
---
+++ src/main/java/com/donhk/boot/Main.java
@@ -0,0 +1,7 @@
+package com.donhk.boot;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
